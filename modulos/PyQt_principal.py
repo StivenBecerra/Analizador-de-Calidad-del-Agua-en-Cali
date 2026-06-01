5f1ae2f71ae7bbbc8e2f5979518d7a5c9ca8761a
niserranoc <niserranoc@unal.edu.co>
2026-05-31 20:42:15 -0500

    Update PyQt_principal.py

--- modulos/PyQt_principal.py
+++ modulos/PyQt_principal.py
@@ -1,17 +1,22 @@
 from PyQt5.QtWidgets import *
 import sys
 def main():
+    #crear ventana
     app = QApplication(sys.argv)
     win = QWidget()
     win.resize(800,600)
     win.setWindowTitle('Analizador de Calidad del Agua en Cali')
-    layout = QHBoxLayout()
-    btn = QPushButton('Enviar')
-    Entre_1= QPushButton("Funcionalidades Entrega 1")
-    Entre_2= QPushButton("Funcionalidades Entrega 2")
+    layout = QGridLayout()
 
-    for i in [Entre_1, Entre_2]:
-        layout.addWidget(i)
+    #Agregar botones
+    Fun= QLabel("Funcionalidades Disponibles:")
+    Entre_1= QPushButton("Entrega 1")
+    Entre_2= QPushButton("Entrega 2")
+    layout.addWidget(Entre_1, 1, 0 )
+    layout.addWidget(Entre_2, 1, 1)
+    layout.addWidget(Fun, 0, 0)
+
     win.setLayout(layout)
     win.show()
     sys.exit(app.exec_())
+main()
